1f69892884df3a4ac8ef1f3448894bbdebcad768
Saikumar Kasaboina <sk19306@corp.ojas-it.com>
2020-08-07 20:24:26 +0530

    theming

--- src/app/dashboard/dashboard.page.ts
+++ src/app/dashboard/dashboard.page.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardPage implements OnInit {
 
-  constructor() { }
+  constructor(private menuCtrl:MenuController) {
+    this.menuCtrl.enable(true); // or true 
+   }
 
   ngOnInit() {
   }
+  slideOpts = {
+    autoplay: {
+    delay: 1500
+    }
+  };
 
 }
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { MenuController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginPage implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router,private menuCtrl: MenuController) { }
 
   ngOnInit() {
+    this.menuCtrl.enable(false); // or true 
+
+  }
+  submit(){
+    this.router.navigate(['/singup']);
+
+  }
+  goToDashBoard(){
+    this.router.navigate(['/dashboard']);
+
+
   }
 
 }
